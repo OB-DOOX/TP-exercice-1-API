615d38b69d66b1bee0d8129d55a30f076a4cf91d
OB-DOOX <pixelobed@gmail.com>
2025-11-04 17:15:09 +0100

    changement index

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+app.use(express.static("HTMLCSS"));
+
+// Routes API
 const courses = require("./routes/courses");
 app.use("/courses", courses);
 
 app.listen(port, () => {
-  console.log(`serveur en ligne`);
+  console.log(`Serveur en ligne sur http://localhost:${port}`);
 });
